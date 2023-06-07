7a91fb4b697ed188094b2ced97f752381a5bcb57
RehanKumarwhitehatjr <97792906+RehanKumarwhitehatjr@users.noreply.github.com>
2023-06-07 11:21:08 +0530

    Add files via upload

---
+++ project-c87/project-c87/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDp6XWz64fSy17c1-tlosquqANuy33H0TI",
+  authDomain: "spectagram-9f9e6.firebaseapp.com",
+  databaseURL: "https://spectagram-9f9e6-default-rtdb.firebaseio.com",
+  projectId: "spectagram-9f9e6",
+  storageBucket: "spectagram-9f9e6.appspot.com",
+  messagingSenderId: "93393476019",
+  appId: "1:93393476019:web:32d23894e1f8df8c7993c6"
+};
